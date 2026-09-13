fe67e5e15f36f834215fd8fd8252ea141f561c5b
Zehra <za2023602@gmail.com>
2026-09-13 20:06:02 +0200

    Update security experiment;Style;Security Checks

--- frontend/app.js
+++ frontend/app.js
@@ -1,69 +1,173 @@
-//Entra-Anwendung Konfigurieren
+console.log("APP.JS WIRD AUSGEFÜHRT");
+//Configuration for MSAL.js
 const msalConfig = {
     auth: {
-        clientId: "4fa3c2d0-7cfc-44f8-b3ef-98e62b50a762", //Entra-Anwendungs-ID
-        authority: "https://login.microsoftonline.com/a5c0f00c-1e40-41ef-ab28-b6412a667908", //Entra-Tenant, bei dem AUthentifizierung stattfindet
-        redirectUri: "http://localhost:3001", //Rückkehradresse nach Authentifizierung bei ENtra
+        clientId: "4fa3c2d0-7cfc-44f8-b3ef-98e62b50a762", //Entra Application (Client) ID
+        authority: "https://login.microsoftonline.com/a5c0f00c-1e40-41ef-ab28-b6412a667908", //Entra Tenant-ID
+        redirectUri: "http://localhost:3001", // Redirect adress for the frontend application after successful login. This must match the redirect URI configured in the Entra application.
 
 
     }
 };
-//MSAL initialisieren -> Browser ist public Client
+//initialize MSAL.js instance
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
-//Definition eigener API
-//-> für OAuth relevant. Nur angemeldete Benutzer mit gültigem Access Token können auf die API zugreifen 
+//Definition of the API scope and URL
+//->  relevant for Oauth. Only the scopes defined in the Entra application can be used. The scope must match the one that is checked in the FastAPI application.
 const apiScope =
     "api://4fa3c2d0-7cfc-44f8-b3ef-98e62b50a762/access_as_user"; //access_as_user ist die Berechtigung, die in der Entra-Anwendung definiert wurde. Sie muss mit der Berechtigung übereinstimmen, die in der FastAPI-Anwendung überprüft wird.  
-const apiUrl = "http://localhost:8000/api/profile"; //FastAPI-Endpunkt, der aufgerufen werden soll
-
+const apiUrl = "http://localhost:8000/api/profile"; //FastAPI-Endpoint for native resource server access. This endpoint is protected by the Entra application and by the FastAPI application and requires a valid access token to access user data.
 
 const loginButton = document.getElementById("login");
 const statusElement = document.getElementById("status");
-
+let experimentEnabled = false;
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+//Authentication - Login with Microsoft
 loginButton.addEventListener("click", async () => {
     try {
         loginButton.disabled = true;
-
-        // 1. Benutzer bei Microsoft anmelden ->OIDC-Flow
+        //- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // 1. Sign in User with Microsoft ->OIDC-Flow and authorize to read user data from Microsoft Graph
+        //- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         const response = await msalInstance.loginPopup({
-            scopes: ["User.Read"], //Microsoft Graph-Berechtigung, um Benutzerinformationen abzurufen
+            scopes: ["User.Read"],  // User.Read is a Microsoft Graph delegated permission.
+            // It allows the SPA to request an access token for Microsoft Graph
+            // and access the signed-in user's profile via the /me endpoint.
+            // The scope must match the one that is checked in the FastAPI application.
         });
 
         console.log("Login erfolgreich:", response);
+        markSuccess("authCard");
 
+        //Display user Information
         statusElement.textContent =
             "Angemeldet als: " + response.account.username;
 
-        // 2. Einholen des Access Token für eigene API
+        await delay(1000);
+
+        // 2. Request an Microsoft Graph
+        const graphTokenResponse = await msalInstance.acquireTokenSilent({
+            scopes: ["User.Read"],
+            account: response.account
+        });
+
+        const graphResponse = await fetch(
+            "https://graph.microsoft.com/v1.0/me",
+            {
+                headers: {
+                    Authorization: `Bearer ${graphTokenResponse.accessToken}`
+                }
+            }
+        );
+
+        const graphData = await graphResponse.json();
+
+        console.log("Microsoft Graph:", graphData);
+
+        document.getElementById("graphName").textContent =
+            "Name: " + graphData.displayName;
+
+        document.getElementById("graphUsername").textContent =
+            "Benutzername: " + graphData.userPrincipalName;
+
+        document.getElementById("graphEmail").textContent =
+            "E-Mail: " +
+            (graphData.mail || "Keine E-Mail-Adresse vorhanden");
+
+        if (graphResponse.ok && graphData) {
+            markSuccess("graphCard");
+        }
+
+
+
+        await delay(1000);
+
+
+
+        //- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // 3. Get Access Token for FastAPI API
+        //- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         const tokenResponse = await msalInstance.acquireTokenSilent({
             scopes: [apiScope],
             account: response.account
         });
-
+        //- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // Display Access Token Claims and Access Token for FastAPI
+        // tokenResponse.accessToken contains the JWT token that can be used to access the FastAPI API.
+        //  The token is a JSON Web Token (JWT) that contains claims about the user and the application. 
+        // The claims can be decoded to get information about the user and the application.
+        //- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         const token = tokenResponse.accessToken;
+        // Decode the JWT token to get the claims
         const payload = JSON.parse(atob(token.split('.')[1]));
         console.log("Access Token Claims:", payload);
         console.log("Access Token für FastAPI:", tokenResponse.accessToken);
 
-
+        //
+        console.log("Sende Token an FastAPI");
+        console.log("Token vorhanden:", !!tokenResponse.accessToken);
         const apiResponse = await fetch(apiUrl, {
             headers: {
                 Authorization: `Bearer ${tokenResponse.accessToken}`
             }
         });
 
-        const data = await apiResponse.json();
+        if (apiResponse.status === 200) {
 
-        console.log("Antwort von FastAPI:", data);
+            // FastAPI hat den Access Token akzeptiert.
+            // Damit war die serverseitige Tokenprüfung erfolgreich.
+            markSuccess("securityCard");
 
-    } catch (error) {
-        console.error("Fehler:", error);
+            await delay(1000);
+            if (!experimentEnabled) {
+                experimentEnabled = true;
+                console.log("Experiment gestartet");
+                // Resource is now availible: the user test lab
+                window.open(
+                    "experiment.html",
+                    "SecurityExperiment",
+                    "width=1200,height=800"
+                );
+            }
 
-        statusElement.textContent =
-            "Fehler: " + error.message;
 
-    } finally {
-        loginButton.disabled = false;
+            const data = await apiResponse.json();
+
+            console.log("Antwort von FastAPI:", data);
+
+            // Profilinformationen anzeigen
+            document.getElementById("result").textContent =
+                JSON.stringify(data, null, 2);
+
+            // FastAPI-Card erfolgreich
+            markSuccess("fastAPICard");
+
+
+
+
+        } else {
+
+            console.log(
+                "FastAPI hat den Access Token abgelehnt:",
+                apiResponse.status
+            );
+        }
     }
+    catch (error) {
+        console.error("Fehler:", error);
+        statusElement.textContent = "Fehler: " + error.message;
+    }
+    finally { loginButton.disabled = false; }
 });
+
+// Function to flash the success card
+function markSuccess(cardId) {
+    const card = document.getElementById(cardId);
+    card.classList.remove("success");
+
+    void card.offsetWidth;
+
+    card.classList.add("success");
+}
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import jwt
 import requests # for requesting public keys from entra
+from jwt import PyJWKClient
+from pydantic import BaseModel
+
+
+TENANT_ID = "a5c0f00c-1e40-41ef-ab28-b6412a667908"
+
+ISSUER = "https://sts.windows.net/a5c0f00c-1e40-41ef-ab28-b6412a667908/"
+
+JWKS_URL = "https://login.microsoftonline.com/a5c0f00c-1e40-41ef-ab28-b6412a667908/discovery/v2.0/keys"
+
+API_AUDIENCE = "api://4fa3c2d0-7cfc-44f8-b3ef-98e62b50a762"
+
+jwks_client = PyJWKClient(JWKS_URL)
 
 app = FastAPI() 
 
@@ -9,17 +22,97 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3001"],
     allow_credentials=True,
-    allow_methods=["*"], #Kritik?
-    allow_headers=["*"], #Kritik?
+    allow_methods=["*"], #Risk?
+    allow_headers=["*"], #Risk ?
 )
+
+
+def validate_access_token(token: str):
+
+    signing_key = jwks_client.get_signing_key_from_jwt(token)
+
+    claims = jwt.decode(
+        token,
+        signing_key.key,
+        algorithms=["RS256"],
+        audience=API_AUDIENCE,
+        issuer=ISSUER,
+    )
+
+    return claims
+
+
+class SecurityTestRequest(BaseModel):
+    issuer: str
+    audience: str
+    scope: str
+
+
+@app.post("/api/security-test")
+def security_test(test: SecurityTestRequest):
+
+    return {
+        "received": True,
+        "issuer": test.issuer,
+        "audience": test.audience,
+        "scope": test.scope
+    }
+
+
 @app.get("/")
 
 def home():
     return {"message": "Willkommen zur Fast.API-App!"}
 
 @app.get("/api/profile")
-def profile(authorization:str | None = Header(default=None)):
-    print("Authorization:", authorization)
+def profile(authorization: str | None = Header(default=None)):
+
+    # 1. Check if the Authorization header is present
+    if authorization is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Authorization Header fehlt"
+        )
+
+    # 2. Check if the Authorization header starts with "Bearer "
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(
+            status_code=401,
+            detail="Ungültiges Authorization-Format"
+        )
+
+    # 3. Take the token from the Authorization header
+    token = authorization.split(" ", 1)[1]
+
+    # 4. Validate the token and extract claims
+    try:
+        claims = validate_access_token(token)
+
+    except Exception as e:
+        print("TOKEN VALIDIERUNG FEHLER:", repr(e))
+
+        raise HTTPException(
+            status_code=401,
+            detail="Ungültiges Access Token"
+        )
+
+    # 5. Prüfen, ob der erforderliche Scope vorhanden ist
+    scopes = claims.get("scp", "").split()
+
+    if "access_as_user" not in scopes:
+        raise HTTPException(
+            status_code=403,
+            detail="Erforderlicher Scope fehlt"
+        )
+
+    # 6. Nur wenn alle Prüfungen erfolgreich waren:
     return {
-        "message": "FastAPI hat den Authorization-Header erhalten"
+        "message": "Access Token ist gültig",
+        "user": claims.get("name"),
+        "scope": claims.get("scp")
     }
+    
+    
+#@app.post("/api/security-test")
+
+ 
